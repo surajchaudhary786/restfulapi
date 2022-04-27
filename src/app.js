bb11c4086624092aa42612a1d13cdcd0ff106cb7
surajchaudhary786 <78081225+surajchaudhary786@users.noreply.github.com>
2022-04-27 23:18:52 +0530

    POST method added, Onclicking submit button (find out more)

--- src/app.js
+++ src/app.js
@@ -1,17 +1,38 @@
-const express = require("express");    //we require express as a function
-require("./db/conn");
-const Student= require("./models/students");
+const express =  require("express");                //we require express as a function
+      require    ("./db/conn");                     //connecting database
+const path    =  require("path");                   //This is included to get __dir work
+const hbs     = require("hbs");                     //to get handlebars working
 
-const app = express();                  // to use express
-const port = process.env.PORT || 8000 ;  //it will give solid port no. for further excution 
-const studentRouter = require("./router/student")
+const app     = express();                          // to use express
+const port    = process.env.PORT || 8000 ;          //it will give solid port no. for further excution 
+
+const static_path   = path.join(__dirname,"../public");
+const template_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
+
+app.use(express.static(static_path));                   //this will redirect to hbs handeler index.hbs
+app.set("view engine","hbs");                           // it denotes which one we using pug,hbs,or cors
+app.set("views",template_path);                         // our views is changed to template_path
+hbs.registerPartials(partials_path) 
+
+app.use(express.json());                               //to able to read json files
+app.use(express.urlencoded({extended:false}));         //to able to read data coming in html format
+
+app.get("/",(req, res)=>{
+    res.render("index")
+});
 
 //we need to create route 
+//--student route-----------------------------------------------
+const studentRouter = require("./router/student")      //referencing student router
+app.use(studentRouter);                                //to able to use router(student)  
 
-app.use(express.json());               //to able to read json files
-app.use(studentRouter);                //to able to use router
+//--teacher route----------------------------------------------
+// const TeacherRouter = require("./router/teacher")      //referencing Teacher router
+// app.use(teacherRouter);                                //to able to use router(Teacher) 
 
 
+//This opens the port for data transmission ; 
 app.listen(port,()=>{
     console.log(`connection is setup at ${port}`);
 })
--- src/router/student.js
+++ src/router/student.js
@@ -1,67 +1,72 @@
 const express = require("express");
 const router = new express.Router();
-const Student = require("../models/students");           // this is to import db so that student model is accessible here
+const Student = require("../models/students"); // this is to import db so that student model is accessible here
 
 //this is to get whole students details....
-router.get("/students",async(req,res)=>{
-    try{
-      const studentsData = await Student.find();         //return promise in future
-      res.send(studentsData);
-    }catch(e){
-        res.status(400).send(e);
-    }
-})
+router.get("/students", async (req, res) => {
+  try {
+    //   const studentsData = await Student.find();         //return promise in future
+    //   res.send(studentsData);
+    res.render("register"); //this will render the registion page
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 
 //this is to get unique student daetails using id (usinf find by id method)
-router.get("/students/:id",async (req,res)=>{
-    try{
-        const _id = req.params.id;
-        const studentData = await Student.findById(_id);
+router.get("/students/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const studentData = await Student.findById(_id);
 
-        if(!studentData){
-            return res.status(404).send();
-        }
-        else{
-            res.send(studentData);
-        }
-    }catch(e){
-        res.status(500).send(e);
+    if (!studentData) {
+      return res.status(404).send();
+    } else {
+      res.send(studentData);
     }
-})
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
 
-router.post("/students",(req,res)=>{
-        const addingstudent=new Student(req.body);        //creating object of the table student (basically a row)
-        console.log(req.body);
-        addingstudent.save().then(()=>{                   //saving that row in side the student table
-            res.status(201).send(`ok received ${addingstudent}`);             //this is handled using promises
-        }).catch((e)=>{
-            res.status(400).send(e);
-        })
-})
+router.post("/students", (req, res) => {
+  const addingstudent = new Student({
+    name: req.body.firstname,                 //because name:"firstname" in form---redirecting and saving
+    email: req.body.email,                    //redirecting and saving
+  });                                         //creating object of the table student (basically a row)
+  addingstudent
+    .save()
+    .then(() => {                             //saving that row in side the student table
+      res.render("index");                    //this is handled using promises
+    })
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
 //update the students by id
-router.patch("/students/:id", async (req,res)=>{
-    try{
-        const _id = req.params.id;
-        const studentupdate = await Student.findByIdAndUpdate(_id,req.body,{
-            new : true
-        });
-        res.send(studentupdate)
-    }catch(e){
-        res.status(404).send(updateStudents);
-    }
-})
+router.patch("/students/:id", async (req, res) => {
+  try {
+    const _id = req.params.id;
+    const studentupdate = await Student.findByIdAndUpdate(_id, req.body, {
+      new: true,
+    });
+    res.send(studentupdate);
+  } catch (e) {
+    res.status(404).send(updateStudents);
+  }
+});
 
-router.delete("/students/:id", async(req,res)=>{
-    try{
-      const id = req.params.id;
-      const deleteStudent = await Student.findByIdAndDelete(id);
-      if(!id){
-          return res.status(400).send();
-      }
-      res.send("deleted");
-    }catch(e){
-          return res.status(500).send(e);
+router.delete("/students/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const deleteStudent = await Student.findByIdAndDelete(id);
+    if (!id) {
+      return res.status(400).send();
     }
-})
+    res.send("deleted");
+  } catch (e) {
+    return res.status(500).send(e);
+  }
+});
 
-module.exports = router ; 
+module.exports = router;
